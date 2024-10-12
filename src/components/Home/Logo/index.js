288afc2d124785ffb42841fbe50a24ec769e45a8
Uzair Mustafa <uzairmustafa@gmail.com>
2024-10-13 00:37:46 +0500

    removed U gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,41 +1,12 @@
 import './index.scss';
 import LogoS from '../../../assets/images/u_logo.png';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial';
 
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export const Logo = () => {
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      });
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    );
-  }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
